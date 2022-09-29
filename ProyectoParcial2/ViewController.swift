87e030b74c67c6545bab60d74e439a500e6ec9ee
analourdescr <ana_lourdes98@hotmail.com>
2022-09-29 08:37:30 -0700

    Initial Commit

---
+++ ProyectoParcial2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoParcial2
+//
+//  Created by Alumno on 9/29/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
